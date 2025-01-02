41ea3b24c09207796826f164054ec6f2380fb039
Punit100643 <ps0477580@gmail.com>
2025-01-02 06:48:42 +0000

    conf

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = 101
+let accountEmail = "punit@google.com"
+var accountPassward = "1234"
+accountCity = "sanwar"
+
+console.log(accountId);
+// in table "[]" is important in "console.table"
+console.table([accountId,accountEmail,accountPassward,accountCity]);
