9b81b76c3b92e5e45f59e20d006866232dd48fd7
Punit100643 <ps0477580@gmail.com>
2025-01-03 04:52:14 +0000

    converting datatype operation

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,15 @@
+let score = "33"
+
+console.log(typeof score);
+
+let stringnumber = Number(score)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
+
+let find = true
+
+console.log(find)
+let boolnumber = Number(find)
+console.log(boolnumber)
