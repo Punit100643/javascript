1831191568ea2ac2fd7f7448562321b38cf5bd39
Punit100643 <ps0477580@gmail.com>
2025-01-03 06:37:00 +0000

    oper

--- 01_basic/02_datatype.js
+++ 01_basic/02_datatype.js
@@ -1,15 +1,31 @@
 let score = "33"
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let stringnumber = Number(score)
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
 
 
 
 let find = true
 
-console.log(find)
+// console.log(find)
 let boolnumber = Number(find)
-console.log(boolnumber)
+// console.log(boolnumber)
+
+// ***********************operation***************************
+
+
+console.log(2 + 2)
+console.log(2 - 2)
+console.log(2 * 2)
+console.log(2 / 2)
+console.log(2 % 2)
+console.log(2**3)  //her Exponentiation (**) is use to give power to the 2   
+
+
+
+let student = 100
+--student
+console.log(student)
