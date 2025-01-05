7154d2883dc6cfdb94e523320d8979fd24d60963
Punit100643 <ps0477580@gmail.com>
2025-01-05 15:03:35 +0000

    array

---
+++ 01_basic/05_array.js
@@ -0,0 +1,24 @@
+let arr = [0,1,2,3,4,5]
+console.log(arr);
+// these are some method in the array
+// console.log(arr.push(6));    it(push) add a value in the array
+
+// console.log(arr.pop());   it(pop) delete  the last  value in the array
+
+// console.log(arr.indexOf(4));   use to find the index of a element inthe array
+
+// console.log(arr.length);
+// console.log(arr.includes(4));   (maybe)use to find the element is present in the array or not.
+// arr.shift(7)
+// console.log(arr.unshift(9)); lnserts new elements at the start of an array, and returns the new length of the array.
+// console.log(arr);
+
+
+const newarr = arr.join()
+console.log(newarr);
+console.log(typeof newarr)
+
+
+
+ 
+
