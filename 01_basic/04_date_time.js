88ba6137ba15c2fd3da49f5493bb1b40f044dc76
Punit100643 <ps0477580@gmail.com>
2025-01-05 07:34:42 +0000

    date and time method

---
+++ 01_basic/04_date_time.js
@@ -0,0 +1,13 @@
+let mydate = new Date()
+console.log(mydate.toString());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toLocaleTimeString());
+console.log(mydate.toISOString());
+console.log(mydate.toUTCString());
+console.log(mydate.getDate());
+console.log(mydate.getHours());
+
+
+
+
+
