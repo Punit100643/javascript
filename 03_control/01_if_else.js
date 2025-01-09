de0dae9e6234751fcebeaf5ae2330c1ee56ba6c7
Punit100643 <ps0477580@gmail.com>
2025-01-09 04:41:51 +0000

    control flow

---
+++ 03_control/01_if_else.js
@@ -0,0 +1,25 @@
+ const score = 80
+
+ 
+// if (score == 100) {
+//     console.log(`you score ${score} in exam `);
+// }
+// else{
+//     console.log(`you score less then ${score} in exam `);
+// }
+
+
+ if (score <=60) {
+    console.log("you score  60");
+}
+else if (score <=70) {
+    console.log("you score  70");
+}
+else if (score <=80) {
+    console.log("you score  80");
+}
+else{
+    console.log(`you score 100 in exam`);
+}
+
+
---
+++ 03_control/02_swithch.js
@@ -0,0 +1,30 @@
+// syntex of switch control statement
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// } 
+
+
+
+const month = "march"
+ switch (month) {
+    case "january":
+        console.log("january");
+         break;
+    case "feb":
+        console.log("feb");
+         break;
+    case "march":
+        console.log("march");
+         break;
+        
+ 
+    default:
+        console.log("nothing from this");
+        
+        break;
+ }
