cd360a404ffc26e5f5d183940ddb8e87ba73862a
Punit100643 <ps0477580@gmail.com>
2025-01-24 16:01:43 +0000

    practice

---
+++ 10_practice/01_pr.js
@@ -0,0 +1,42 @@
+// Write a program to print numbers from 1 to 10.
+// Write a program to check if a given number is odd or even.
+// Create an array of your favorite colors and print each color using a loop.
+// Write a program to reverse a string.
+// Write a program that calculates the factorial of a given number.
+// Create an object with your personal details (name, age, city) and print each value.
+// Write a program to find the largest number in an array.
+// Write a program that checks if a given string is a palindrome.
+// Create a function that converts Celsius to Fahrenheit.
+
+
+
+
+
+// Q1
+// for (let i = 1; i <=10; i++) {
+    //     // 10 element = array[i];  
+    //     console.log(i);
+    // }
+
+// Q2
+    // let num = 11
+
+    // if(num  % 2 === 0){
+    //     console.log("given number is even");
+    // }
+    // else{
+    //     console.log("given number is odd");
+    // }
+
+// Q3
+    // let arr = ["red","green","yellow","brown","orange"];
+    // for (let i = 0; i < arr.length; i++) {
+        //     // const element = arr[i];
+        //     console.log(arr[i])
+        // }
+        
+        // Q4
+        
+// let arr = ["red","green","yellow","brown","orange"];
+// let  reversed = arr.reverse();
+// console.log(reversed);
